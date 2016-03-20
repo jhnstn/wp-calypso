83264740c3dec37cfb2346c8130cc0663a39cac0
bluefuton <chris@bluefuton.com>
2016-03-20 22:20:52 +1100

    Add followList redux action

--- client/state/reader/lists/actions.js
+++ client/state/reader/lists/actions.js
@@ -6,7 +6,13 @@ import {
 	READER_LISTS_RECEIVE,
 	READER_LISTS_REQUEST,
 	READER_LISTS_REQUEST_SUCCESS,
-	READER_LISTS_REQUEST_FAILURE
+	READER_LISTS_REQUEST_FAILURE,
+	READER_LISTS_FOLLOW,
+	READER_LISTS_FOLLOW_SUCCESS,
+	READER_LISTS_FOLLOW_FAILURE,
+	READER_LISTS_UNFOLLOW,
+	READER_LISTS_UNFOLLOW_SUCCESS,
+	READER_LISTS_UNFOLLOW_FAILURE,
 } from 'state/action-types';
 
 /**
@@ -26,7 +32,7 @@ export function receiveLists( lists ) {
 /**
  * Triggers a network request to fetch the current user's lists.
  *
- * @return {Function}        Action thunk
+ * @return {Function}        Action promise
  */
 export function requestSubscribedLists() {
 	return ( dispatch ) => {
@@ -46,6 +52,43 @@ export function requestSubscribedLists() {
 					dispatch( receiveLists( data.lists ) );
 					dispatch( {
 						type: READER_LISTS_REQUEST_SUCCESS,
+						data
+					} );
+					resolve();
+				}
+			} );
+		} );
+	};
+}
+
+/**
+ * Triggers a network request to follow a list.
+ *
+ * @param  {String}  owner List owner
+ * @param  {String}  slug List slug
+ * @return {Function} Action promise
+ */
+export function followList( owner, slug ) {
+	return ( dispatch ) => {
+		dispatch( {
+			type: READER_LISTS_FOLLOW,
+			owner,
+			slug
+		} );
+
+		const query = { owner, slug };
+
+		return new Promise( ( resolve, reject ) => {
+			wpcom.undocumented().followList( query, ( error, data ) => {
+				if ( error || ! ( data && data.following ) ) {
+					dispatch( {
+						type: READER_LISTS_FOLLOW_FAILURE,
+						error
+					} );
+					reject();
+				} else {
+					dispatch( {
+						type: READER_LISTS_FOLLOW_SUCCESS,
 						data
 					} );
 					resolve();
