7c2b121fcda9f9c611e9d0e3bfff52aca77c2bb0
Matías Ventura <mv@matiasventura.com>
2016-03-22 10:49:14 +0100

    Add event tracking for upgrade nudge.

--- client/components/upgrade-nudge/index.jsx
+++ client/components/upgrade-nudge/index.jsx
@@ -10,6 +10,7 @@ import noop from 'lodash/noop';
  */
 import Button from 'components/button';
 import Gridicon from 'components/gridicon';
+import analytics from 'analytics';
 
 export default React.createClass( {
 
@@ -19,21 +20,32 @@ export default React.createClass( {
 		onClick: React.PropTypes.func,
 		className: React.PropTypes.string,
 		message: React.PropTypes.string,
-		icon: React.PropTypes.string
+		icon: React.PropTypes.string,
+		event: React.PropTypes.string
 	},
 
 	getDefaultProps() {
 		return {
 			onClick: noop,
 			message: 'And get your own domain address.',
-			icon: 'star'
+			icon: 'star',
+			event: null
 		}
+	},
+
+	handleClick() {
+		if ( this.props.event ) {
+			analytics.tracks.recordEvent( 'calypso_upgrade_nudge_cta_click', {
+				cta_name: this.props.event
+			} );
+		}
+		this.props.onClick();
 	},
 
 	render() {
 		const classes = classNames( this.props.className, 'upgrade-nudge' );
 		return (
-			<Button className={ classes } onClick={ this.props.onClick }>
+			<Button className={ classes } onClick={ this.handleClick }>
 				<div className="upgrade-nudge__info">
 					<span className="upgrade-nudge__title">
 						{ this.translate( 'Upgrade to Premium' ) }
